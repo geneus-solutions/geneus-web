6d743b349dfa23547ca15235829252f2c585170b
geneus-solutions <geneus.solutionsit@gmail.com>
2024-10-27 22:01:11 +0530

    update env

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
-export const backendUrl = "https://geneus-solutions-backend.onrender.com";
+export const backendUrl = "https://geneus-api.onrender.com";
 export const razorpayScript = "https://checkout.razorpay.com/v1/checkout.js";
 export const googlesc = "https://accounts.google.com/gsi/client";
 // prettier-ignore
