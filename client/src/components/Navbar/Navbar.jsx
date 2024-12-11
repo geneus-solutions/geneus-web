cb82b7b836610274c560c4a4a3a5a2a62f3cc69c
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2024-12-11 17:20:40 +0530

    add admin menu section in header and only show for admin

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,49 +1,56 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import "./Navbar.css";
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import { IoMdArrowDropdown } from "react-icons/io";
 
 // import { useAuthenticateQuery } from '../../features/authenticate/authenticateApiSlice';
 import { MDBIcon } from "mdbreact";
 import { MDBBadge } from "mdbreact";
-import { useDispatch } from 'react-redux';
-import { useAuthenticateQuery } from '../../features/authenticate/authenticateApiSlice';
-import { useCartQuery } from '../../features/Cart/cartApiSlice';
-import { Cart } from '../../features/Cart/cartSlice';
+import { useDispatch } from "react-redux";
+import { useAuthenticateQuery } from "../../features/authenticate/authenticateApiSlice";
+import { useCartQuery } from "../../features/Cart/cartApiSlice";
+import { Cart } from "../../features/Cart/cartSlice";
 
 function Navbar() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const {data} = useAuthenticateQuery();
-  const {data:cartData} = useCartQuery(data?.data?.id,{skip:!data?.data?.id});
-  
+  const { data } = useAuthenticateQuery();
+  console.log("this is user", data);
+  const { data: cartData } = useCartQuery(data?.data?.id, {
+    skip: !data?.data?.id,
+  });
+
   useEffect(() => {
-    if(cartData){
-      dispatch(Cart({cart:cartData}));
+    if (cartData) {
+      dispatch(Cart({ cart: cartData }));
     }
-  }, [cartData])
+  }, [cartData, dispatch]);
+
+  // for admin role
+  const isAdmin = data?.data?.role === "admin"; //We can change from here like if you want to change 
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
       <div className="container-fluid">
         {/* Logo Section */}
         <NavLink to="/" className="navbar-brand">
-          <img 
-            src="https://www.geneussolutions.in/static/media/GSMainLogo.e373ff51a56528f216e6.png" 
-            alt="Logo" 
+          <img
+            src="https://www.geneussolutions.in/static/media/GSMainLogo.e373ff51a56528f216e6.png"
+            alt="Logo"
             className="logo-img"
           />
         </NavLink>
 
         {/* Toggler Button for Mobile View */}
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNav" 
-          aria-controls="navbarNav" 
-          aria-expanded="false" 
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
@@ -53,17 +60,21 @@ function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <NavLink 
-                to="/" 
-                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
               >
                 Home
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink 
-                to="/about" 
-                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+              <NavLink
+                to="/about"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
               >
                 About
               </NavLink>
@@ -71,52 +82,79 @@ function Navbar() {
 
             {/* Services Dropdown */}
             <li className="nav-item dropdown">
-              <div className="nav-link dropdown-toggle" id="servicesDropdown" >
-                Services
+              <div className="nav-link" id="servicesDropdown" >
+                Services <IoMdArrowDropdown className="dropdown-icon"/>
               </div>
               <div className="dropdown-menu" aria-labelledby="servicesDropdown">
-                <NavLink 
-                  to="/courses" 
-                  className="dropdown-item"
-                >
+                <NavLink to="/courses" className="dropdown-item">
                   Courses
                 </NavLink>
-                <NavLink 
-                  to="/nutri-app" 
-                  className="dropdown-item"
-                >
+                <NavLink to="/nutri-app" className="dropdown-item">
                   Nutri App
                 </NavLink>
               </div>
             </li>
 
+            {/* Admin Dropdown */}
+            {isAdmin && (
+              <li className="nav-item dropdown">
+                <div className="nav-link" id="adminDropdown">
+                  Admin Menu <IoMdArrowDropdown className="dropdown-icon"/>
+                </div>
+                <div className="dropdown-menu" aria-labelledby="adminDropdown">
+                  <NavLink to="/add-course" className="dropdown-item">
+                    Add Course
+                  </NavLink>
+                  <NavLink to="/add-product" className="dropdown-item">
+                    Add Product
+                  </NavLink>
+                </div>
+              </li>
+            )}
+
             <li className="nav-item">
-              <NavLink 
-                to="/contact" 
-                className={({ isActive }) => isActive ? "nav-link active" : "nav-link"}
+              <NavLink
+                to="/contact"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
               >
                 Contact
               </NavLink>
             </li>
             <li className="nav-item">
-              <NavLink to='/login'>
-              {data?.data?.id ? <div>
-                <p style={{marginTop: "10px"}}>{data?.data?.name}</p>
-              </div>:
-              <div className="login-button" >
-                <p style={{marginTop: '10px'}}>Login</p>
-              </div>}
+              <NavLink to="/login">
+                {data?.data?.id ? (
+                  <div>
+                    <p style={{ marginTop: "10px" }}>{data?.data?.name}</p>
+                  </div>
+                ) : (
+                  <div className="login-button">
+                    <p style={{ marginTop: "10px" }}>Login</p>
+                  </div>
+                )}
               </NavLink>
-             
             </li>
-            <li className="nav-item" onClick={()=>navigate('/cart')} style={{display:'flex',justifyContent:'center',alignItems:'center',marginLeft:'20px',cursor:'pointer'}}>
+            <li
+              className="nav-item"
+              onClick={() => navigate("/cart")}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                marginLeft: "20px",
+                cursor: "pointer",
+              }}
+            >
               <h4 className="linkText3">
                 <MDBIcon icon="shopping-cart" size="x" />
                 <MDBBadge
-                    // color="primary"
-                    className="badge-notification"
+                  // color="primary"
+                  className="badge-notification"
                 >
-                  {cartData?.cart_items?.length ? cartData?.cart_items?.length : 0}
+                  {cartData?.cart_items?.length
+                    ? cartData?.cart_items?.length
+                    : 0}
                 </MDBBadge>
               </h4>
             </li>
