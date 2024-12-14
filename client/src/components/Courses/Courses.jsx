dd1792fc13e8fb9de530234bc707a8624e761ebe
Rahul Pharthyal <rahulpharthyal04@gmail.com>
2024-12-14 12:09:27 +0530

    there are too many changes on ui on course page and nabar and make some changes on signup and login page and notification showing after succesfull registration and login and when you add course to cart and remove the course from cart hahandle the notifacation

--- client/src/components/Courses/Courses.jsx
+++ client/src/components/Courses/Courses.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import img1 from "../../assets/banner.jpeg";
 import "./Course.css";
-import { Link } from "react-router-dom";
 import { useCourcesQuery } from "../../features/cources/courceApiSlice";
+import CourseCard from "./CourseCard";
+import CourseBanner from "./CourseBanner";
 
 const Course = ({ searchResults }) => {
   const [selectedOption, setSelectedOption] = useState("");
@@ -34,14 +35,29 @@ const Course = ({ searchResults }) => {
     (course) => course.level === "advanced" && course.enabled === true
   );
 
+  const displayedCourses =
+    searchResults?.length > 0
+      ? searchResults
+      : selectedOption === "beginner"
+      ? beginnerCourses
+      : selectedOption === "intermediate"
+      ? intermediateCourses
+      : selectedOption === "advanced"
+      ? advancedCourses
+      : courses?.filter((course) => course?.enabled === true);
+
+      const course = {
+        title: "Course",
+        description: "There are many course Listed here select the course as your choice"
+      }
+
   return (
+
     <div>
-      <div className="banner-container">
-        <img src={img1} className="banner-image" alt="Contact Banner" />
-        <div className="banner-overlay">
-          <h1 className="banner-text">Courses</h1>
-        </div>
-      </div>
+      <CourseBanner
+      imgSrc={img1}
+      course={course}
+      />
       <div className="dropdown">
         <button className="dropbtn">
           {selectedOption || "Categories"}
@@ -49,43 +65,16 @@ const Course = ({ searchResults }) => {
         </button>
         <div className="dropdown-content">
           <span onClick={() => handleDropdownChange("beginner")}>Beginner</span>
-          <span onClick={() => handleDropdownChange("intermediate")}>Intermediate</span>
+          <span onClick={() => handleDropdownChange("intermediate")}>
+            Intermediate
+          </span>
           <span onClick={() => handleDropdownChange("advanced")}>Advanced</span>
         </div>
       </div>
 
       <div className="courses-container">
-        {(searchResults?.length > 0
-          ? searchResults
-          : selectedOption === "beginner"
-          ? beginnerCourses
-          : selectedOption === "intermediate"
-          ? intermediateCourses
-          : selectedOption === "advanced"
-          ? advancedCourses
-          : courses?.filter((course) => course?.enabled === true)
-        )?.map((course) => (
-          <div className="course-card" key={course?.id}>
-            <img src={course?.img} alt={course?.title} className="course-image" />
-            <div className="course-body">
-              <h3 className="course-title">{course?.title}</h3>
-              <div className="rating">
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="fas fa-star"></i>
-                <i className="far fa-star"></i>
-              </div>
-              <p className="course-price">
-                <s>₹{course?.price}</s> <strong className="text-danger">₹{course?.discount_price}</strong>
-              </p>
-              <div className="text-center">
-                <Link to={`/course/${course?._id}`}>
-                  <button className="buy-now-btn">Buy Now</button>
-                </Link>
-              </div>
-            </div>
-          </div>
+        {displayedCourses?.map((course) => (
+          <CourseCard key={course?.id} course={course} />
         ))}
       </div>
     </div>
