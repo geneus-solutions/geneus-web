461c0eac659aa3073693cf5c81ef65ad93b14fa7
rahulsinghpharthyal <rahulpharthyal04@gmail.com>
2025-10-05 20:33:59 +0530

    change on course_description_image

--- client/src/components/Courses/DescriptionCourseCard.jsx
+++ client/src/components/Courses/DescriptionCourseCard.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './DescriptionCourseCard.css';
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./DescriptionCourseCard.css";
 
-const DescriptionCourseCard = ({ courseDetails, discount, handleAddToCart }) => {
+const DescriptionCourseCard = ({
+  courseDetails,
+  discount,
+  handleAddToCart,
+}) => {
   const navigate = useNavigate();
   return (
     <div className="course-row">
-        <div className="course-col">
+      <div className="course-col">
         <img
           src={courseDetails?.img}
           alt="Course"
           className="course-image"
-          style={{ height: '350px' }}
         />
       </div>
       <div className="course-col">
@@ -19,32 +22,34 @@ const DescriptionCourseCard = ({ courseDetails, discount, handleAddToCart }) => 
           <h2 className="course-title">{courseDetails?.title}</h2>
           <div className="course-details">
             <h6>
-              <s>
-                &#8377;{courseDetails?.price}
-              </s>
-              &ensp;{parseInt(discount)}% OFF 
-            <strong className="course-price">{" "}
-              &#8377;{courseDetails?.discount_price}
-            </strong>
+              <s>&#8377;{courseDetails?.price}</s>
+              &ensp;{parseInt(discount)}% OFF
+              <strong className="course-price">
+                {" "}
+                &#8377;{courseDetails?.discount_price}
+              </strong>
             </h6>
           </div>
-          <div style={{display:'flex',textAlign: 'left',gap:'20px'}}>
-              <button
-                className="add-to-cart-button"
-                onClick={() => navigate('/course-details', { state: { courses:[courseDetails] } })}
-              >
-                Buy Now
-              </button>
-              <button
-                className="add-to-cart-button"
-                onClick={() => handleAddToCart(courseDetails)}
-              >
-                Add to Cart
-              </button>
+          <div style={{ display: "flex", textAlign: "left", gap: "20px" }}>
+            <button
+              className="add-to-cart-button"
+              onClick={() =>
+                navigate("/course-details", {
+                  state: { courses: [courseDetails] },
+                })
+              }
+            >
+              Buy Now
+            </button>
+            <button
+              className="add-to-cart-button"
+              onClick={() => handleAddToCart(courseDetails)}
+            >
+              Add to Cart
+            </button>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
